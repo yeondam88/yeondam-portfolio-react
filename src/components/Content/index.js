d082a3c956d04a12852e86002757cec697c7453e
Yeondam Park <giyen9088@gmail.com>
2018-09-10 19:17:17 -0700

    extracting data to its own file..

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
 import img from "../../img/paper-plane.svg";
+import posed from "react-pose";
+
+const Text = posed.strong({
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+});
 
 const texts = ["a Front End Engineer", "a JavaScript Lover"];
 
 class Content extends Component {
   state = {
-    textIdx: 0
+    textIdx: 0,
+    isVisible: true
   };
 
   componentDidMount() {
-    this.timeout = setInterval(() => {
-      let currentIdx = this.state.textIdx;
-      this.setState({ textIdx: currentIdx + 1 });
+    setInterval(() => {
+      this.setState({ isVisible: !this.state.isVisible });
     }, 3000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timeout);
   }
 
   render() {
     let textThatChanges = texts[this.state.textIdx % texts.length];
-
+    const { isVisible } = this.state;
     return (
       <section className="content">
         <div className="content__about">
           <h1>
             I'm Yeondam Park
             <br />
-            <strong className="tagline">{textThatChanges}</strong>
+            <Text className="tagline" pose={isVisible ? "visible" : "hidden"}>
+              a Front End Engineer
+            </Text>
+            <Text className="tagline" pose={isVisible ? "hidden" : "visible"}>
+              a JavaScript Lover
+            </Text>
           </h1>
           <p>
             Love to building a beautiful things with <strong>JavaScript</strong>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,66 +1,10 @@
 import React, { Component } from "react";
+import { portfolioData } from "../../data";
 
 class Portfolio extends Component {
   state = {
     isActive: false,
-    portfolios: [
-      {
-        id: 1,
-        title: "item 1",
-        path:
-          "https://cdn.dribbble.com/users/25686/screenshots/5140212/dribbble.jpg",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 2,
-        title: "item 2",
-        path:
-          "https://cdn.dribbble.com/users/44585/screenshots/5140880/wolverine-dribbble.png",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 3,
-        title: "item 3",
-        path:
-          "https://cdn.dribbble.com/users/44585/screenshots/4959706/iron-man-dribbble.png",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 4,
-        title: "item 4",
-        path:
-          "https://cdn.dribbble.com/users/44585/screenshots/5021576/thor.png",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 5,
-        title: "item 5",
-        path:
-          "https://cdn.dribbble.com/users/25686/screenshots/4810050/dribbble.jpg",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 6,
-        title: "item 6",
-        path:
-          "https://cdn.dribbble.com/users/1205252/screenshots/4853796/catrinas.png",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 7,
-        title: "item 7",
-        path:
-          "https://cdn.dribbble.com/users/1205252/screenshots/4845230/erkaeva-shop.png",
-        description: "item 1 is made with React and Redux."
-      },
-      {
-        id: 8,
-        title: "item 8",
-        path:
-          "https://cdn.dribbble.com/users/44585/screenshots/4926663/bug-lab-bee-ig.jpg.png",
-        description: "item 1 is made with React and Redux."
-      }
-    ],
+    portfolios: portfolioData,
     activePortfolio: {}
   };
 
